f746de71d4681d783c161344e7794ff795f0c3ad
Andressa Regina Lopes Celestino <andressa.lopes.090c@gmail.com>
2024-02-27 23:56:58 +0000

    using fragments

---
+++ app/src/main/java/com/alopes/eleventhmodule/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.alopes.eleventhmodule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.alopes.eleventhmodule.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding.startActivityButton.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToGameNav()
+            findNavController().navigate(action)
+        }
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/alopes/eleventhmodule/MainActivity.kt
+++ app/src/main/java/com/alopes/eleventhmodule/MainActivity.kt
@@ -1,62 +1,102 @@
 package com.alopes.eleventhmodule
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.alopes.eleventhmodule.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawer: DrawerLayout
+    lateinit var navDrawer: NavigationView
+    lateinit var bottomNav: BottomNavigationView
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        val toolbar = binding.toolbar
+        //val activityResultIntent = Intent(this, GameResultActivity::class.java)
+
+        //binding.bottomNav
+        //startActivity(activityResultIntent)
 
         setContentView(binding.root)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.toolbar2)
 
-        //supportActionBar?.title = "@+Id"
-        supportActionBar?.setLogo(R.drawable.ic_logo)
-        supportActionBar?.setDisplayUseLogoEnabled(true)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        drawer = binding.root
+        navDrawer = binding.navView
+        bottomNav = binding.bottomNav
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+       if(savedInstanceState == null) {
+           supportFragmentManager.beginTransaction()
+               .replace(R.id.fragmentContainerView, HomeFragment())
+               .commit()
+       }
 
 
-        val activity2Intent = Intent(this, MainActivity2::class.java)
-        intent.putExtra("name", "Andressa")
-        intent.putExtra("age", 34)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.playerFragment, R.id.resultFragment), drawer)
 
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navDrawer.setupWithNavController(navController)
 
-        val activityResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result: ActivityResult ->
-            if(result.resultCode == Activity.RESULT_OK){
-                val intent = result.data
-                if(intent?.hasExtra("result")== true){
-                    Snackbar.make(
-                        binding.root,
-                        intent.getStringExtra("result") ?: "",
-                        Snackbar.LENGTH_SHORT).show()
+        setupBottomNavigation()
+
+    }
+
+    private fun setupBottomNavigation(){
+        bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.player1 -> {
+                    val intent = Intent(this,PlayerFragment::class.java)
+                    startActivity(intent)
+                    true
                 }
+                R.id.result -> {
+                    val intent = Intent(this,ResultFragment::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
             }
         }
 
-        binding.startActivityButton.setOnClickListener {
-            activityResult.launch(activity2Intent)
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+   override fun onSupportNavigateUp(): Boolean {
+//       drawer.openDrawer(GravityCompat.START)
+      return true
     }
 
+    //       override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    //           menuInflater.inflate(R.menu.second_screen_menu,menu)
+    //          return true
+    //       }
 
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when(item.itemId){
+
+//            R.id.menu_restrat -> {
+//                val intent = Intent(this,HomeFragment::class.java)
+//                startActivity(intent)
+ //               true
+//            }
+ //           else -> false
+
+ //       }
+
+ //   }
 }
